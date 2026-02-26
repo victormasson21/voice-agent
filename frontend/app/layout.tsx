6ecc0a6882dc1c5b3e47271367cd779885311840
vmasson-elder <vmasson@elder.org>
2026-02-26 17:30:13 +0000

    Switch off night mode for now

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -65,12 +65,7 @@ export default async function RootLayout({ children }: RootLayoutProps) {
         <meta name="description" content={pageDescription} />
       </head>
       <body className="overflow-x-hidden">
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
+        <ThemeProvider attribute="class" defaultTheme="light" disableTransitionOnChange>
           {children}
           <div className="group fixed bottom-0 left-1/2 z-50 mb-2 -translate-x-1/2">
             <ThemeToggle className="translate-y-20 transition-transform delay-150 duration-300 group-hover:translate-y-0" />
